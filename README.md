0267fe96e4228ecdcd6d8fc8a750f1273141c116
Tony DiNitto <think41c@gmail.com>
2015-03-23 14:25:49 -0600

    Initial work on a database site for Sinatra.

---
+++ README.md
@@ -0,0 +1,1 @@
+Example of simple database site using Sinatra. 
---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,26 @@
+require 'models/task_manager'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = TaskManager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    TaskManager.create(params[:task])
+    redirect '/tasks'
+  end
+
+end
+
+
